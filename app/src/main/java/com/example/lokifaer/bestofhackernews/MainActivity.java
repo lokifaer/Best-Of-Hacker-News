9c60aee51f580b2554ef867ced27540d84b0ed34
lokifaer <lokifaer@gmail.com>
2016-09-06 17:01:46 +0200

    added progress bar

--- app/src/main/java/com/example/lokifaer/bestofhackernews/MainActivity.java
+++ app/src/main/java/com/example/lokifaer/bestofhackernews/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.oc.hnapp.android.HNQueryTask;
 
@@ -24,7 +26,17 @@ public class MainActivity extends AppCompatActivity
 
         myTask = new HNQueryTask(myAdapter, 80, 1);
         myTask.execute();
+
+        final ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
+        myAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver()
+        {
+            @Override
+            public void onChanged() {
+                progress.setVisibility(View.GONE);
+            }
+        });
     }
+
 
     @Override
     protected void onDestroy()
